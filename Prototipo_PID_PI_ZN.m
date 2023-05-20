130375b102347662927f82294eda7d308d19d510
Nélio Dias <jrneliodias@gmail.com>
2023-05-19 21:00:10 -0300

    update ziegler Nichols

--- Prototipo_PID_PI_ZN.m
+++ Prototipo_PID_PI_ZN.m
@@ -1,20 +1,22 @@
 %% Controle Proporcional Integral Sintonizado por Ziegler Nichols
-clear all; close all; clc
+%clear all; close all; clc
 
 %% ----- Condições iniciais
 nit = 800; ts = 0.01;
-angulo_sensor(1:nit) = 0; %u(1:nit) = '0,0\n';
-erro(1:nit) = 0;
+angulo_sensor = zeros(1,nit); 
+erro = zeros(1,nit);
 
 % %% ----- Referência
-angulo_ref(1:nit)  = 50;
-% angulo_ref(1:nit/2)  = 80;
-% angulo_ref(nit/2 +1 : nit)  = 20;
+angulo_ref = 50*ones(1,nit);
+% angulo_ref(1:nit/3)  = 50;
+% angulo_ref(nit/3 +1 : 2*nit/3)  = 50;
+% angulo_ref(2*nit/3 +1 : 3*nit/3)  = 50;
 
 % %% ----- Variável Controlada
-pot_motor_1(1:nit)  = 0;
-pot_motor_2(1:nit) = 0;
+pot_motor_1 = zeros(1,nit);
+pot_motor_2 = zeros(1,nit);
 u = strings(1,nit); u(1:nit) = "0,0";
+
 
 % Iniciar o Protótipo
 start = input("Start Daqduino? ","s");
@@ -34,9 +36,9 @@ theta_zn1 = thetasmith1;
 Kp_zn1 = Kpsmith1;
 
 % Coefiientes do Modelo Smith motor 2
-Kpsmith2 = 12.86;
-thetasmith2 =0.5;
-tausmith2 =0.66;
+Kpsmith2 =  12.86;
+thetasmith2 = 0.5;
+tausmith2 =   0.66;
 
 tau_zn2   = tausmith2;
 theta_zn2 = thetasmith2;
@@ -48,9 +50,11 @@ Kp1 = 1.2*tau_zn1/(Kp_zn1*theta_zn1);
 Ti1 = 2*theta_zn1;
 Td1 = theta_zn1/2;
 
+ajuste_kd = 0.01;
+
 % Cálculo Ki1 e Kd1
 Ki1 = ts*Kp1/Ti1;
-Kd1 = 0.05*(Kp1*Td1)/ts;
+Kd1 = ajuste_kd*(Kp1*Td1)/ts;
 
 % Sintonia do PID da Tabela ZN para o motor 2
 Kp2 = 1.2*tau_zn2/(Kp_zn2*theta_zn2);
@@ -60,7 +64,7 @@ Td2 = theta_zn2/2;
 
 % Cálculo Ki2 e Kd2
 Ki2 = ts*Kp2/Ti2;
-Kd2 = 0.05*Kp2*Td2/ts;
+Kd2 = ajuste_kd*Kp2*Td2/ts;
 
 % Sintonia PI
 
@@ -139,63 +143,57 @@ daqduino_write(u0,ts);
 
 
 %% ----- Plotar sinais
+metodo = 'Ziegler e Nichols';
+ajuste = ' com ajuste';
+
 t = 0:ts:(nit-1)*ts;
 figure(1)
-plot(t(2:nit),angulo_sensor(2:nit),'r',t(2:nit),angulo_ref(2:nit),'--k'),grid
-title("Controle Proporcional-Integral")
-legend('Real')
+p = plot(t,angulo_sensor,'r',t,angulo_ref,'--k');grid
+title(["Controle PID sintonizado por " metodo ajuste])
+
 ylim([0,90])
 
+% Settling criteria
+desired_range = [0.95*angulo_sensor(end), 1.05*angulo_sensor(end)];  % Desired steady-state range
+tolerance = 0.01;             % Tolerance for settling range
+
+% Find the indices where the response is within the desired range
+settling_indices = find(angulo_sensor > desired_range(1) & angulo_sensor < desired_range(2));
+
+% Calculate the settling time
+settling_time = t(settling_indices(1)) - t(1);
+
+% Rise time calculation
+
+rise_9_index = findidx(angulo_sensor,0.9*angulo_sensor(end));
+rise_1_index = findidx(angulo_sensor,0.1*angulo_sensor(end));
+rise_time = t(rise_9_index(1)) - t(rise_1_index(1));
+
+% Plot the settling time point
+hold on;
+
+
+plot(t(settling_indices(1)), angulo_sensor(settling_indices(1)), 'b.','MarkerSize',20);
+text(t(settling_indices(1)), angulo_sensor(settling_indices(1))+5,rise_time, ['Settling Time = ', num2str(settling_time)],...
+    'VerticalAlignment', 'bottom','HorizontalAlignment','center','Color','b');
+% Plot the rise time point
+
+plot(t(rise_9_index), angulo_sensor(rise_9_index), 'k.','MarkerSize',20);
+text(t(rise_9_index)+0.2, angulo_sensor(rise_9_index)+0.5, ['Rise Time = ', num2str(rise_time)], 'VerticalAlignment', 'top','HorizontalAlignment','left');
+
+legend('Real','Referência')
+
+
+% Potência dos Motores
 figure(2)
 subplot(211)
 plot(t,pot_motor_1)
 ylim([6,16])
 grid
-title("Potência do Motor 1")
+title(['Potência do Motor 1 - ' metodo ajuste])
 
 subplot(212)
 plot(t,pot_motor_2)
 ylim([6,16])
 grid
-title("Potência do Motor 2")
-
-
-%
-%
-%
-% finish = input("Close Daqduino? ","s");
-% if finish =="y"
-%     daqduino_end; % End the connection to the DaqDuino device.
-% end
-
-
-
-
-
-
-
-
-
-
-
-
-
-%         angulo_sensor(k) = daqduino_read;
-%         erro(k) = angulo_ref(k) - angulo_sensor(k);
-%         if(erro(k)>0)
-%             pot_motor_1(k) = 10;
-%             pot_motor_2(k) = 7;
-%         else
-%             pot_motor_1(k) = 7;
-%             pot_motor_2(k) = 10;
-%         end
-%
-%         if (pot_motor_1(k)> 12) || (pot_motor_2(k) >12)
-%             u = [num2str(7),',',num2str(7),'\n'];
-%             daqduino_write(u,ts);
-%         else
-%             u = [num2str(pot_motor_1(k)),',',num2str(pot_motor_2(k)),'\n'];
-%             daqduino_write(u,ts); % D/A to analog output, and delaying Ts seconds.
-%
-%
-%         end
+title(['Potência do Motor 2 - ' metodo ajuste])
--- Rascunho.m
+++ Rascunho.m
@@ -16,3 +16,8 @@ daqduino_write(u,ts);
 
 
 daqduino_end
+
+str = '-2,-7599';
+numbers = strsplit(str, ',');
+number1 = str2double(numbers{1});
+number2 = str2double(numbers{2});
